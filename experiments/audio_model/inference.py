0b69073ac06d79bb22b6e3f5f101906e0bab057b
ankithsavio <ankithsavio@gmail.com>
2024-12-07 23:45:37 +0000

    add init audio-to-text

--- experiments/audio_model/inference.py
+++ experiments/audio_model/inference.py
@@ -1,14 +1,21 @@
 from faster_whisper import WhisperModel
 
+
 def inference():
 
-    model_size = 'large-v2'
+    model_size = "large-v2"
 
     model = WhisperModel(model_size, device="cuda", compute_type="float16")
 
-    segments, info = model.transcribe("audio.mp3", beam_size=5)
+    segments, info = model.transcribe(
+        "/home/ubuntu/project_truth/TruthTok/experiments/video_model/data/audios/sample_audio.mp3",
+        beam_size=5,
+    )
 
-    print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+    print(
+        "Detected language '%s' with probability %f"
+        % (info.language, info.language_probability)
+    )
 
     for segment in segments:
         print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
