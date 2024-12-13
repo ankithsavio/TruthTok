0fdf37bcbed19171ef7041ae64c7a4fd03125809
ankithsavio <ankithsavio@gmail.com>
2024-12-13 17:06:16 +0000

    update video generator - fix error

--- experiments/video_model/segment_generator.py
+++ experiments/video_model/segment_generator.py
@@ -9,15 +9,28 @@ def segment_video_generator(input_file, segment_time=60):
     cap = cv2.VideoCapture(input_file)
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    total_duration = frame_count // fps
 
+    segment_counter = 0
     chunk_frames = []
-    start_time = 0
-    frame_indices = list(range(frame_count - 1))
-    sample_indices = [
-        frame_indices[i]
-        for i in frame_sample(total_duration, mode="uniform", num_frames=16)
-    ]  # 16 frames for videollama2.1
+    frame_indices = list(range(frame_count))
+
+    segment_frame_count = segment_time * fps
+
+    def update_segment_indices(counter):
+        start_index = counter * segment_frame_count
+        end_index = min((counter + 1) * segment_frame_count, frame_count)
+        segment_slice = slice(start_index, end_index)
+
+        indices = [
+            frame_indices[segment_slice][i]
+            for i in frame_sample(
+                end_index - start_index, mode="uniform", num_frames=16
+            )
+        ]  # 16 frames for videollama2.1
+
+        return indices
+
+    sample_indices = update_segment_indices(segment_counter)
 
     frame_index = 0
     while cap.isOpened():
@@ -25,14 +38,20 @@ def segment_video_generator(input_file, segment_time=60):
         if not ret:
             break
 
-        current_time = len(chunk_frames) / fps + start_time
         if frame_index in sample_indices:
             chunk_frames.append(frame)
 
-        if current_time >= start_time + segment_time or not ret:
-            yield chunk_frames
+        if frame_index >= (segment_counter + 1) * segment_frame_count or not ret:
+            if chunk_frames:
+                yield chunk_frames
+            segment_counter += 1
+            if segment_counter * segment_frame_count < frame_count:
+                sample_indices = update_segment_indices(segment_counter)
             chunk_frames = []
-            start_time += segment_time
 
         frame_index += 1
+
+    if chunk_frames:
+        yield chunk_frames
+
     cap.release()
