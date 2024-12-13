6f67d182e1a5887395b03de6464929330fa8cea3
ankithsavio <ankithsavio@gmail.com>
2024-12-13 11:44:04 +0000

    update generator to sample 16 frames at a time - compatible with videollama2.1

--- experiments/video_model/segment_generator.py
+++ experiments/video_model/segment_generator.py
@@ -1,4 +1,5 @@
 import cv2
+from videollama2.mm_utils import frame_sample
 
 
 def segment_video_generator(input_file, segment_time=60):
@@ -12,18 +13,26 @@ def segment_video_generator(input_file, segment_time=60):
 
     chunk_frames = []
     start_time = 0
+    frame_indices = list(range(frame_count - 1))
+    sample_indices = [
+        frame_indices[i]
+        for i in frame_sample(total_duration, mode="uniform", num_frames=16)
+    ]  # 16 frames for videollama2.1
 
+    frame_index = 0
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
 
         current_time = len(chunk_frames) / fps + start_time
-        chunk_frames.append(frame)
+        if frame_index in sample_indices:
+            chunk_frames.append(frame)
 
         if current_time >= start_time + segment_time or not ret:
             yield chunk_frames
             chunk_frames = []
             start_time += segment_time
 
+        frame_index += 1
     cap.release()
